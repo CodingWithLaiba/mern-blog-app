099fdcab57fe9bd98d37ad1863cb36d1de81471d
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-22 14:14:35 +0500

    Export GlobalContext and enhance GlobalState component with additional state management for blogList and pending status

--- Client/src/context/index.jsx
+++ Client/src/context/index.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import { createContext } from "react";
 
-const GlobalContext = createContext(null);
+export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
-    const [formData,setFormData] = useState({
-        title="",
-        description:"",
-    });
-  return <GlobalContext.Provider value={{formData,setFormData}}>{children}</GlobalContext.Provider>;
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+  const [blogList, setBlogList] = useState([]);
+  const [pending, setPending] = useState(false);
+  return (
+    <GlobalContext.Provider
+      value={{
+        formData,
+        setFormData,
+        pending,
+        setPending,
+        blogList,
+        setBlogList,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
